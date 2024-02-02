18ecc7a8c2c2d4710bdea9e00c1c30d177cd2a20
@mubasher.skylinxtech <mubasher.skylinxtech@gmail.com>
2024-02-03 01:44:37 +0500

    changed style

--- server/db.js
+++ server/db.js
@@ -1,4 +1,4 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
 export async function initializeDatabase() {
   //   let dbURL = process.env.DATABASE_ONLINE;
--- server/server.js
+++ server/server.js
@@ -1,17 +1,17 @@
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 
-import { createRequire } from 'module';
+import { createRequire } from "module";
 
-import app from './app.js';
-import { initializeDatabase } from './db.js';
-import asyncHandler from './utils/asyncHandler.js'; // module to use environment file
+import app from "./app.js";
+import { initializeDatabase } from "./db.js";
+import asyncHandler from "./utils/asyncHandler.js"; // module to use environment file
 dotenv.config();
 const require = createRequire(import.meta.url);
 
 // IT SHOULD BE ON TOP SO THAT WE CATCH EVERY ERROR
 // SOLVING UNCAUGHT EXCEPTION (for example a variable that is undefined)
-process.on('uncaughtException', (err) => {
-  console.log('UNCAUGHT EXCEPTION. Shutting Down! 🤦‍♂️');
+process.on("uncaughtException", (err) => {
+  console.log("UNCAUGHT EXCEPTION. Shutting Down! 🤦‍♂️");
   console.log(err.name, err.message);
   console.log(err);
   // 0 for success
@@ -21,18 +21,18 @@ process.on('uncaughtException', (err) => {
 
 const initializeServer = asyncHandler(async () => {
   const dbConnection = await initializeDatabase();
-  console.log('DB CONNECTION SUCCESSFULL');
+  console.log("DB CONNECTION SUCCESSFULL");
   // console.log('DB CONNECTION: ', dbConnection);
 
   const port = process.env.PORT || 8000;
 
   const server = app.listen(port, () => {
-    console.log('Listening at port: ', port);
+    console.log("Listening at port: ", port);
   });
 
   // SOLVING UNHANDLED REJECTION (catch promise rejections etc)
-  process.on('unhandledRejection', (err) => {
-    console.log('🤷‍♂️ UNHANDLED REJECTION. Shutting Down! 🤦‍♂️');
+  process.on("unhandledRejection", (err) => {
+    console.log("🤷‍♂️ UNHANDLED REJECTION. Shutting Down! 🤦‍♂️");
     console.log(err.name, err.message);
     console.log(err);
     server.close(() => {
@@ -42,10 +42,10 @@ const initializeServer = asyncHandler(async () => {
     });
   });
 
-  process.on('SIGTERM', () => {
-    console.log('👌 SIGTERM RECIEVED. Shutting Down! 🤦‍♂️');
+  process.on("SIGTERM", () => {
+    console.log("👌 SIGTERM RECIEVED. Shutting Down! 🤦‍♂️");
     server.close(() => {
-      console.log('💥🔥 Process terminated.');
+      console.log("💥🔥 Process terminated.");
     });
   });
 });
